0e54a0ddb34270e7ded108836efa148f953d8808
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-10-17 20:28:16 +0200

    Create 20230920_fv_gyak.py

---
+++ 20230920_fv_gyak.py
@@ -0,0 +1,16 @@
+def Add(x,y):
+    return x+y
+
+def Minus(x,y):
+    return x-y
+
+def Mul(x,y):
+    return x*y
+
+def Mod(x,y):
+    return x % y
+
+print(Add(5,7))
+print(Minus(7,5))
+print(Mul(2,3))
+print(Mod(4,3))
